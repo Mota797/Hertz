35acbd0a1693300ed2229c514c2aa62390fbf3b6
Mota797 <joaomota0808200@gmail.com>
2026-01-30 15:04:54 -0300

    adicionando teste do model de confirmacao compra e removendo o campo status

--- confirmar_compra_app/models.py
+++ confirmar_compra_app/models.py
@@ -9,16 +9,16 @@ class Confirmacao_Compra(models.Model):
         ('cartao_de_credito', 'Cartão de Crédito'),
         ('cartao_de_debito', 'Cartão de Débito')
     ]
-    STATUS_COMPRA_CHOICES = [
-        ('em_andamento', 'Em Andamento'),
-        ('finalizada', 'Finalizada'),
-        ('cancelada', 'Cancelada')
-    ]
+    # STATUS_COMPRA_CHOICES = [
+    #     ('em_andamento', 'Em Andamento'),
+    #     ('finalizada', 'Finalizada'),
+    #     ('cancelada', 'Cancelada')
+    # ]
     usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True, related_name='usuario_compras')
     metodo_pagamento = models.CharField(choices=METODOS_PAGAMENTO_CHOICES)
     carrinho = models.ForeignKey(Carrinho_Compras, on_delete=models.CASCADE) #SET_DEFAULT, default=)
     data_compra = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(choices=STATUS_COMPRA_CHOICES)
+    # status = models.CharField(choices=STATUS_COMPRA_CHOICES)
 
 class Item_Compra(models.Model):
     compra = models.ForeignKey(Confirmacao_Compra, on_delete=models.CASCADE, related_name='itens')
--- confirmar_compra_app/tests/tests_model.py
+++ confirmar_compra_app/tests/tests_model.py
@@ -1,19 +1,42 @@
-from ..models import Confirmacao_Compra, Usuario, Carrinho_Compras, Item_Compra
-from carrinho_app.models import Quantidade_Item_Carrinho, Produto
+from confirmar_compra_app.models import Confirmacao_Compra, Item_Compra
+from carrinho_app.models import Quantidade_Item_Carrinho, Produto, Carrinho_Compras
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from decimal import Decimal
 
 class ConfirmacaoCompraTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.usuario = Usuario.objects.create(username="Carlos",email="Carlos@email.com", first_name="Carlos", last_name="Silva")
-        cls.carrinho = Carrinho_Compras.objects.create(usuario=cls.usuario)
-        cls.produto = Produto.objects.create()
-        cls.quantidade_item_carrinho = Quantidade_Item_Carrinho.objects.create(carrinho=cls.carrinho)
 
+        Usuario = get_user_model()
+        cls.usuario = Usuario.objects.create(username="Carlos",email="Carlos@email.com", password='uruguai', first_name="Carlos", last_name="Silva")
+        cls.produto = Produto.objects.create(nome='microfone JBL', preco=5999, quantidade_estoque=90, categoria='microfone', imagem='padrao.png')
+        cls.carrinho = Carrinho_Compras.objects.create(usuario=cls.usuario)
+        cls.quantidade_item_carrinho = Quantidade_Item_Carrinho.objects.create(carrinho=cls.carrinho, produto=cls.produto, quantidade=2)
+        
     def setUp(self):
         self.confirmacao_compra = Confirmacao_Compra.objects.create(
             usuario = self.usuario,
             metodo_pagamento = 'cartao_de_credito',
-            carrinho = 1,
-
+            carrinho = self.carrinho,
             )
+        
+        self.item_compra = Item_Compra.objects.create(
+            compra = self.confirmacao_compra,
+            produto = self.produto,
+            quantidade_comprada = 2,
+            preco_unitario = self.produto.preco_decimal()
+        )
+        
+    def test_compra_confirmada(self):
+        self.assertEqual(self.usuario.username,'Carlos','O nome de usuário está incorreto')
+        self.assertEqual(self.confirmacao_compra.metodo_pagamento,'cartao_de_credito','O método de pagamento está incorreto')
+        self.assertEqual(self.carrinho.usuario.username,'Carlos','O nome de usuário do está incorreto')
+
+    def test_item_compra(self):
+        self.assertEqual(self.confirmacao_compra.usuario.username,'Carlos','O nome do usuário está incorreto')
+        self.assertEqual(self.produto.nome,'microfone JBL','O produto está incorreto')
+        self.assertEqual(self.item_compra.quantidade_comprada,2,'A quantidade comprada está incorreta')
+        self.assertEqual(self.item_compra.preco_unitario, self.produto.preco_decimal(), 'O preco unitario está incorreto')
+        self.assertEqual(self.item_compra.subtotal, Decimal("119.98"), 'O subtotal está incorreto')
+
