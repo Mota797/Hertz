4f8ac399dcf67e4f59ae6440481d77465f738738
Mota797 <joaomota0808200@gmail.com>
2026-02-11 17:01:10 -0300

    fazendo uma barra de pesquisa uhuu

--- hertz_app/views.py
+++ hertz_app/views.py
@@ -3,11 +3,21 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Produto
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 class home(ListView):
     model = Produto
     template_name = 'home.html'
     context_object_name = 'produtos'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('pesquisar')
+        object_list = Produto.objects.all()
+
+        if query:
+            object_list = object_list.filter(Q(nome__icontains=query) | Q(categoria__icontains=query))
+        
+        return object_list
 
 class detail_product(DetailView):
     model = Produto
