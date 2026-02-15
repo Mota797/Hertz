cb876b918684a87b3e2d5e5af67b6948b0021b82
Mota797 <joaomota0808200@gmail.com>
2026-02-15 18:28:05 -0300

    finalmente fiz o carrossel funcionar (ufaa)

--- hertz_app/static/js/carrossel.js
+++ hertz_app/static/js/carrossel.js
@@ -1,43 +1,35 @@
 const botao_esquerda = document.getElementById('passar_esquerda');
 const botao_direita = document.getElementById('passar_direita');
 
-// const imagem1 = document.getElementById('img_carrossel_produto1');
-// const imagem2 = document.getElementById('img_carrossel_produto2');
-// const imagem3 = document.getElementById('img_carrossel_produto3');
+const imagem1 = document.getElementById('img_carrossel_produto1');
+const imagem2 = document.getElementById('img_carrossel_produto2');
+const imagem3 = document.getElementById('img_carrossel_produto3');
 
-const lista_carrossel = [document.getElementById('img_carrossel_produto1'), document.getElementById('img_carrossel_produto2'), document.getElementById('img_carrossel_produto3')];
+const lista_carrossel = document.getElementById('lista_carrossel_detalhe');
+const array_carrossel = [imagem1, imagem2, imagem3]
 
 let i = 0;
 
 botao_esquerda.onclick = function () {
-
-    lista_carrossel[i].style.transform = 'translateX(0%)'
-    if (i < lista_carrossel.length && i != 0){
-        i -= 1
+    if (i < array_carrossel.length && i != 0){
+        i = i - 1
     }
     else if (i == 0){
-        i = (lista_carrossel.length - 1)
+        i = (array_carrossel.length - 1)
     }
-    
-    lista_carrossel[i].style.transform = `translateX(calc(${i} * 100%))`
-    console.log(i)
+    lista_carrossel.style.transform = `translateX(-${i * 100}%)`
+    console.log('esquerda')
 }
 
 botao_direita.onclick = function () {
-    console.log(lista_carrossel[i])
-    lista_carrossel[i].style.transform = 'translateX(0%)'
-    if (i == (lista_carrossel.length - 1)){
+    if (i == (array_carrossel.length - 1)){
         i = 0
-        console.log(i)
     }
 
-    else if (i <= (lista_carrossel.length - 1)) {
+    else if (i <= (array_carrossel.length - 1)) {
         i += 1
-        console.log(i)
     }
 
-    lista_carrossel[i].style.transform = `translateX(calc(${i} * -100%))`
-    console.log(i)
+    lista_carrossel.style.transform = `translateX(-${i * 100}%)`
+    console.log('direita')
 }
-
-console.log(i)
